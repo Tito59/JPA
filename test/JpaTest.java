ffb044afb11e638341fee6e740c4db6e1d0c7c87
Formation <Formation@10.18.0.74>
2017-11-14 13:57:32 +0100

    v1.0

---
+++ test/JpaTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.Test;
+
+/**
+ *
+ * @author Formation
+ */
+public class JpaTest {
+    
+    @Test
+    public void JpaTest() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PU");
+    }
+    
+}
